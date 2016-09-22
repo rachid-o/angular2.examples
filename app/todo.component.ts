6d3fb4b5c3a6ee7f495ba489ddee59c1db8d85d1
Rachid Ben Moussa <rachid.benmoussa@planonsoftware.com>
2016-09-22 13:53:25 +0200

    Show status of in sync with backend

--- app/todo.component.ts
+++ app/todo.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 
-import { TodoService } from './todo.service';
+import { Todo, TodoService } from './todo.service';
 
 @Component({
     templateUrl: 'app/todo.component.html',
@@ -9,35 +9,33 @@ import { TodoService } from './todo.service';
 
 export class TodoComponent implements OnInit {
 
-    private todos: string[];
-    private todos$: Observable<string[]>;
+    private todos$: Observable<Todo[]>;
     // private singleTodo$: Observable<string>;
-
 
     constructor(private todoService: TodoService) { 
     }
 
-
     ngOnInit() {
-        // this.todos = ['item 1', 'item 2', 'item 3'];
-        // this.todos = [];
         this.todos$ = this.todoService.todos$; // subscribe to entire collection
+        this.todoService.loadAll();
 
         // subscribe to only one single todo
         // this.singleTodo$ = this.todoService.todos$
         //         .map(todos => todos.find(item => item === 'single'));
 
         // this.todoService.load('1');    // load only todo with id of '1'
-
-
     }
 
     loadTodos() {
         console.log('loadTodos()');
-        this.todoService.loadAll();    // load all todos
+        this.todoService.loadAll();
     }
 
     addTodo() {
         this.todoService.addTodo();
+    }
+
+    getColor(todo: Todo): string {
+        return todo.isSync ? 'green' : 'red';
     }
 }
--- app/todo.service.ts
+++ app/todo.service.ts
@@ -4,50 +4,87 @@ import { Subject } from 'rxjs/subject';
 
 // http://blog.angular-university.io/how-to-build-angular2-apps-using-rxjs-observable-data-services-pitfalls-to-avoid/
 
+export interface Todo {
+  id: number;
+  name: string;
+  isSync: boolean;
+}
+
 @Injectable()
 export class TodoService {
 
-
-  private _todos$: Subject<string[]>;
-  private dataStore: string[]
+  private _todos$: Subject<Todo[]>;
+  private dataStore: Todo[] = [];
   private counter: number;
 
   constructor() {
-    this.dataStore = ['item 1', 'single', 'item 3'];
+    this.dataStore = this.generateTodos();
     this.counter = this.dataStore.length;
-    this._todos$ = <Subject<string[]>>new Subject();
+    this._todos$ = <Subject<Todo[]>>new Subject();
   }
 
-
-  get todos$(): Observable<string[]> {
+  get todos$(): Observable<Todo[]> {
     return this._todos$.asObservable();
   }
 
   public loadAll(): void {
+    console.debug('loadAll()...');
     this._todos$.next(this.dataStore);
   }
 
   public addTodo(): void {
     this.counter++;
     // Do some async stuff to add the TODO to backend service. When this is finished update client with its value
-    let newItem = 'added item ' + this.counter;
-    // this.dataStore.push(newItem);
-    this.addTODObackend(newItem).subscribe( resultItem => {
-      this.dataStore.push(resultItem);
+    let newItemName = 'added item ' + this.counter;
+    let newTodo: Todo = { id: this.counter, name: newItemName, isSync: false };
+    // Add it to local cache
+    this.dataStore.push(newTodo);
 
+    // Now add it to backend
+    this.addTODObackend(newTodo).subscribe((resultItem: Todo) => {
+      let updatedTodo = this.findById(resultItem.id);
+      if(updatedTodo) {
+        updatedTodo.isSync = true;
+      }
     });
-    // addTODObackend
-    // this._todos$.next(this.dataStore);
   }
 
-  private addTODObackend(name: string): Observable<string> {
-    return Observable.create((observer:any) => {
-      console.debug('simulate adding item to backend');
+
+  /**
+   * @returns undefined when not found
+   */
+  private findById(id: number): Todo {
+    let retval: Todo = undefined;
+    this.dataStore.forEach((item, index) => {
+      if (item.id === id) {
+        retval = item;
+      }
+    });
+    return retval;
+  }
+
+
+  private addTODObackend(item: Todo): Observable<Todo> {
+    return Observable.create((observer: any) => {
+      console.debug('Simulate adding item to backend, will finish after 2 seconds');
       setTimeout(() => {
-        observer.next(name + ' (saved on server)');
+        // item.isSync = true;
+        observer.next(item);
+        // observer.next(name + ' (saved on server)');
         observer.complete();
-      }, 1000);
+      }, 2000);
     });
   }
+
+
+  private generateTodos(): Todo[] {
+    let todoList: Todo[] = [];
+    for (let n of [1, 2, 3]) {
+      let t: Todo = { id: n, name: 'item ' + n, isSync: true };
+      todoList.push(t);
+    }
+    return todoList;
+  }
+
 
 }
